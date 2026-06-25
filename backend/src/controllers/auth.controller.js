2571c19817a2fe821102c1909ce3c345638aa265
Bhaskar Yogi <shardayogi60@gmail.com>
2026-06-25 10:29:06 +0530

    login refactoring

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -65,10 +65,12 @@ export const signup = async (req, res) => {
 
 export const login = async (req, res) => {
     const { email, password } = req.body;
+
+    if(!email || !password){
+        return res.status(400).json({ message: "All fields are required" })
+    }
+
     try {
-        if(!email || !password){
-            return res.status(400).json({ message: "All fields are required" })
-        }
         const user = await User.findOne({ email });
         if (!user) {
             return res.status(400).json({ message: "Invalid credentials" })
