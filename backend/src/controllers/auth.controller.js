ba3550152bc265aff5d3b0935295b99a5d7427f6
Bhaskar Yogi <shardayogi60@gmail.com>
2026-06-25 10:15:59 +0530

    added login/logout functionality

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -62,3 +62,47 @@ export const signup = async (req, res) => {
         res.status(500).json({ message: "Internal server error" })
     }
 }
+
+export const login = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        if(!email || !password){
+            return res.status(400).json({ message: "All fields are required" })
+        }
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({ message: "Invalid credentials" })
+            //never tell the client which one is incorrect:password or email
+        }
+        const isPasswordCorrect = await bcrypt.compare(password, user.password);
+        if (!isPasswordCorrect) {
+            return res.status(400).json({ message: "Invalid credentials" })
+        }
+        generateToken(user._id, res);
+        res.status(200).json({
+            _id: user._id,
+            fullName: user.fullName,
+            email: user.email,
+            profilePic: user.profilePic,
+        });
+    } catch (error) {
+        console.error("Error in login controller:", error);
+        res.status(500).json({ message: "Internal server error" })  
+    }
+}
+
+export const logout = (_, res) => {
+    res.cookie("jwt", "",{maxAge: 0});
+    res.status(200).json({ message: "Logged out successfully" });
+    // try {
+    //     res.clearCookie("token", {
+    //         httpOnly: true,
+    //         secure: ENV.NODE_ENV === "production",
+    //         sameSite: "strict",
+    //     });
+    //     res.status(200).json({ message: "Logged out successfully" });
+    // } catch (error) {
+    //     console.log("Error in logout controller:", error);
+    //     res.status(500).json({ message: "Internal server error" });
+    // }
+};
