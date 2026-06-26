b22ae6c325dc547c399d4c44d869e21a0299b233
Bhaskar Yogi <shardayogi60@gmail.com>
2026-06-26 10:57:49 +0530

    update profile endpoint added

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import path from 'path';
+import cookieParser from 'cookie-parser';
 
 import authRoutes from './routes/auth.route.js';
 import messageRoutes from './routes/message.route.js';
@@ -15,6 +16,7 @@ const __dirname=path.resolve();
 const PORT= ENV.PORT || 3000;
 
 app.use(express.json())//req.body// to parse incoming JSON data in request bodies
+app.use(cookieParser())//req.cookies//to parse cookies from incoming requests
  
 app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes);
