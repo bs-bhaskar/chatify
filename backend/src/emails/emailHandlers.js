15f8f5c4ea31ff7ff7b1c4d250a3036d4d1489a7
Bhaskar Yogi <shardayogi60@gmail.com>
2026-07-01 09:26:34 +0530

    signup implemented on the frontend

--- backend/src/emails/emailHandlers.js
+++ backend/src/emails/emailHandlers.js
@@ -4,7 +4,7 @@ import { createWelcomeEmailTemplate } from "../emails/emailTemplates.js";
 export const sendWelcomeEmail = async (email, name, clientURL) => {
   const { data, error } = await resendClient.emails.send({
     from: `${sender.name} <${sender.email}>`,
-    to: email, 
+    to: email,  
     subject: "Welcome to Chatify!",
     html: createWelcomeEmailTemplate(name, clientURL),
   });
